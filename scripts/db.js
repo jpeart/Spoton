c7ac0e2ed1f8d57c5173724b15f4a8edd2df8c40
jpeart <jpeart4@gmail.com>
2017-10-07 09:17:36 -0500

    file structure

---
+++ scripts/db.js
@@ -0,0 +1,11 @@
+onst mongoose = require("mongoose");
+const db = require("../models");
+mongoose.Promise = global.Promise;
+
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist",
+//   {
+//     useMongoClient: true
+//   }
+// );
