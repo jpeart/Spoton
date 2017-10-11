cdc7618042b0e294f62173637315745ba0e96da3
Joshua Miller <jlmiller07@gmail.com>
2017-10-10 20:55:37 -0500

    adding react side and tweaks to routes and controller

---
+++ routes/api/index.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const bookRoutes = require("./sugars");
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
-const path = require("path");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 3001;
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
-const apiRoutes = require("./routes/apiRoutes");
+const PORT = process.env.PORT || 3001;
 
+// Configure body parser for AJAX requests
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
+// Add routes, both API and view
+app.use(routes);
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// Set up promises with mongoose
+mongoose.Promise = global.Promise;
+// Connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/spotOn",
+  {
+    useMongoClient: true
+  }
+);
 
-// parse application/json
-app.use(bodyParser.json());
-
-// Use apiRoutes
-app.use("/api", apiRoutes);
-
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
-
+// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> Server now on port ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
