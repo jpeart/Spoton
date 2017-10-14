4c8fb69d233c773c86b4e65822ce6b7dae2ffc04
jpeart <jpeart4@gmail.com>
2017-10-14 11:56:04 -0500

    capitol letters are dumb

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from './components/Login';
+import Login from './components/login/Login';
 
 class App extends Component {
   render() {
